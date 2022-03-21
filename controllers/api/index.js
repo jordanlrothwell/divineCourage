e3ac14f855a0dde98b9c56563bfe058fc76e0947
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-03-21 19:29:01 +1000

    Changed path syntax

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const seedRoutes = require("./seedRoutes");
 
-router.use("/seeds", seedRoutes);
+router.use("/seed", seedRoutes);
 
 module.exports = router;
